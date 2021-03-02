9ee0349073643d96bb0e874cc3e46b807a7dda66
mikepmerritt <mikepmerritt@gmail.com>
2021-03-02 03:27:49 -0500

    added welcome, number assignment, waiting, and final message

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -2,7 +2,20 @@
 public class DmvSimulator {
 
 	public static void main (String[] args) {
+		System.out.println("Hello, welcome to the DMV!");
 		
+		int userNumber = (int) (Math.random() * 100 + 1);
+		System.out.println("Your number is " + userNumber + ". Please wait until your number is called.");
+		
+		for(int calledNumber = userNumber + 1; calledNumber != userNumber; calledNumber++) {
+			if(calledNumber > 100) {
+				calledNumber -= 100;
+			}
+			System.out.println("Now serving: " + calledNumber);
+		}
+		System.out.println("Now serving: " + userNumber);
+		
+		System.out.println("Sorry, we cannot help you, as you do not appear to have the required paperwork.");
 	}
 	
 }
